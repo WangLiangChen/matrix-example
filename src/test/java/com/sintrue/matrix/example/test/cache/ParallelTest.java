d0fd4b56205cfef3fc9ae102dae5764d95f9747b
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-04-07 10:56:50 +0800

    调整部分commons

---
+++ src/test/java/com/sintrue/matrix/example/test/cache/ParallelTest.java
@@ -0,0 +1,19 @@
+package com.sintrue.matrix.example.test.cache;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+
+/**
+ * @author Liangchen.Wang 2021-12-28 16:20
+ */
+public class ParallelTest {
+    public static void main(String[] args) {
+
+    }
+
+}
