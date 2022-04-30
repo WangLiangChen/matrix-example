0200fc1f180ec2b1d314fbb6cba3657602137800
Liangchen.Wang <sintrue@hotmail.com>
2022-04-30 21:07:05 +0800

    DbLock

--- src/test/java/com/sintrue/matrix/example/test/MainTest.java
+++ src/test/java/com/sintrue/matrix/example/test/MainTest.java
@@ -2,7 +2,10 @@ package com.sintrue.matrix.example.test;
 
 import wang.liangchen.matrix.framework.commons.validation.ValidationUtil;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 
 /**
  * @author Liangchen.Wang 2022-04-29 17:37
@@ -10,12 +13,15 @@ import javax.validation.constraints.NotBlank;
 public class MainTest {
     public static void main(String[] args) {
         Staff staff = new Staff();
+        staff.setId(0L);
         ValidationUtil.INSTANCE.validate(staff);
     }
 
     static class Staff {
-        @NotBlank(message = "helell")
         private String name;
+        @Min(value = 1,message = "min")
+        @Null(message = "null")
+        private Long id;
 
         public String getName() {
             return name;
@@ -23,6 +29,14 @@ public class MainTest {
 
         public void setName(String name) {
             this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
         }
     }
 }
