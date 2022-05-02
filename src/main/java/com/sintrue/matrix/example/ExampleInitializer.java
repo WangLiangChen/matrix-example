9b39392a858f73863ed3b66216adb2a8671549a6
Liangchen.Wang <sintrue@hotmail.com>
2022-05-02 08:10:48 +0800

    example

--- src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
+++ src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
@@ -2,13 +2,12 @@ package com.sintrue.matrix.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import wang.liangchen.matrix.framework.data.annotation.EnableJdbc;
 
 /**
  * @author Liangchen.Wang
  */
 @SpringBootApplication
-@EnableJdbc
+//@EnableJdbc
 public class ExampleInitializer {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(ExampleInitializer.class);
---
+++ src/test/java/com/sintrue/matrix/example/test/AuthorizationTests.java
@@ -0,0 +1,10 @@
+package com.sintrue.matrix.example.test;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthorizationTests {
+    @Test
+    public void testAdd() {
+        System.out.println("============");
+    }
+}
