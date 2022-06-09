83075cc303320f7a44af27d31a085e5a82af4290
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-06-09 14:43:48 +0800

    优化 EnableJdbc ...

--- src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
+++ src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
@@ -1,26 +1,36 @@
 package com.sintrue.matrix.example;
 
-import org.apache.commons.configuration2.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import wang.liangchen.matrix.framework.commons.classloader.LoadedScanner;
-import wang.liangchen.matrix.framework.springboot.context.ConfigurationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @author Liangchen.Wang
  */
 @SpringBootApplication
 public class ExampleInitializer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         SpringApplication springApplication = new SpringApplication(ExampleInitializer.class);
         springApplication.run(args);
 
-        Configuration configuration = ConfigurationContext.INSTANCE.resolve("jdbc.properties");
-        String string = configuration.getString("primary.dialect");
-        LoadedScanner.INSTANCE.getJars().forEach(System.out::println);
-        LoadedScanner.INSTANCE.getClasses().forEach(System.out::println);
-        System.out.println(string);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("classpath*:/**/*.class");
+        for (Resource resource : resources) {
+            System.out.println(resource.getURI());
+        }
     }
 }
