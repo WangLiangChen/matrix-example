c7e70f3b3230503eb7349c0f715c4f99b426e26e
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-06-13 20:17:33 +0800

    优化 EnableJdbc ... 修改datatype和javatype的映射

--- src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
+++ src/main/java/com/sintrue/matrix/example/ExampleInitializer.java
@@ -2,21 +2,9 @@ package com.sintrue.matrix.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 /**
  * @author Liangchen.Wang
@@ -26,11 +14,5 @@ public class ExampleInitializer {
     public static void main(String[] args) throws IOException, URISyntaxException {
         SpringApplication springApplication = new SpringApplication(ExampleInitializer.class);
         springApplication.run(args);
-
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        Resource[] resources = resolver.getResources("classpath*:/**/*.class");
-        for (Resource resource : resources) {
-            System.out.println(resource.getURI());
-        }
     }
 }
---
+++ src/test/java/com/sintrue/matrix/example/test/CodeGenerator.java
@@ -0,0 +1,22 @@
+package com.sintrue.matrix.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import wang.liangchen.matrix.framework.data.annotation.EnableJdbc;
+import wang.liangchen.matrix.framework.generator.DomainGenerator;
+
+import javax.inject.Inject;
+
+/**
+ * @author Liangchen.Wang 2022-05-06 9:11
+ */
+@SpringBootTest
+@EnableJdbc
+public class CodeGenerator {
+    @Inject
+    private DomainGenerator domainGenerator;
+    @Test
+    public void doIt() {
+       domainGenerator.build();
+    }
+}
