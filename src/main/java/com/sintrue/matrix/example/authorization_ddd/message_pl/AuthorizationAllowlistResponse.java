c53bb16c2cf1710aced112936ea53d68b3dbae81
Liangchen.Wang <sintrue@hotmail.com>
2022-06-29 15:55:03 +0800

    重新整理Example和IAM

--- src/main/java/com/sintrue/matrix/example/authorization_ddd/message_pl/AuthorizationAllowlistResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sintrue.matrix.example.authorization_ddd.message_pl;
-
-/**
- * @author Liangchen.Wang 2022-06-15 8:23
- */
-public class AuthorizationAllowlistResponse {
-}
--- src/main/java/com/sintrue/matrix/example/authorization_ddd/southbound_acl/port/repository/AuthorizationAllowlistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sintrue.matrix.example.authorization_ddd.southbound_acl.port.repository;
-
-import wang.liangchen.matrix.framework.data.dao.IDao;
-
-/**
- * @author Liangchen.Wang 2022-06-15 7:46
- */
-public interface AuthorizationAllowlistRepository extends IDao {
-}
