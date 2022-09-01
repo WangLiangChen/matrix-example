7531bdac503bd81d97e3a99891c257ac0f3c3bb4
Liangchen.Wang <sintrue@hotmail.com>
2022-09-02 00:19:37 +0800

    增加签名验签 加密解密待增加

--- src/test/java/com/sintrue/matrix/example/test/cache/CommonTest.java
+++ src/test/java/com/sintrue/matrix/example/test/cache/CommonTest.java
@@ -1,46 +1,64 @@
 package com.sintrue.matrix.example.test.cache;
 
 import org.junit.jupiter.api.Test;
-import wang.liangchen.matrix.framework.commons.enumeration.Symbol;
-import wang.liangchen.matrix.framework.commons.exception.MatrixErrorException;
+import wang.liangchen.matrix.framework.commons.encryption.DigestUtil;
+import wang.liangchen.matrix.framework.commons.encryption.KeyPairString;
+import wang.liangchen.matrix.framework.commons.encryption.SecretKeyUtil;
+import wang.liangchen.matrix.framework.commons.encryption.SignatureUtil;
+import wang.liangchen.matrix.framework.commons.encryption.enums.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * @author Liangchen.Wang 2022-08-27 19:18
  */
 public class CommonTest {
     @Test
-    public void testException() {
-        try {
-            handleException();
-        } catch (Exception e) {
-            String message = stackTraceToString(e, new StringBuilder());
-            System.out.println(message);
+    public void testHmac() {
+        String key = "matrix";
+        String data = "matrix_matrix";
+        for (HmacAligorithm aligorithm : HmacAligorithm.values()) {
+            String hmac = DigestUtil.INSTANCE.hmac(aligorithm, key, data);
+            System.out.println(aligorithm + ":" + hmac);
         }
     }
 
-    private void handleException() {
-        boolean doIt = true;
-        try {
-            if (doIt) {
-                throw new RuntimeException("I am RuntimeException");
-            }
-        } catch (Exception e) {
-            throw new MatrixErrorException(e, "I am MatrixErrorException");
+    @Test
+    public void testDigest() {
+        String data = "matrix_matrix";
+        for (DigestAlgorithm aligorithm : DigestAlgorithm.values()) {
+            String hmac = DigestUtil.INSTANCE.digest(aligorithm, data);
+            System.out.println(aligorithm + ":" + hmac);
         }
     }
 
-    private String stackTraceToString(Throwable throwable, StringBuilder messageContainer) {
-        if (null == throwable) {
-            return messageContainer.toString();
+    @Test
+    public void testSign() {
+        String data = "Hello,I am matrix";
+        for (SignatureAlgorithm signatureAlgorithm : SignatureAlgorithm.values()) {
+            System.out.println("签名算法:" + signatureAlgorithm);
+            KeyPairString keyPairString = SecretKeyUtil.INSTANCE.keyPair(signatureAlgorithm.getKeyPairAlgorithm());
+            String privateKey = keyPairString.getPrivateKey();
+            System.out.println("privateKey:" + privateKey);
+            String sign = SignatureUtil.INSTANCE.sign(signatureAlgorithm, privateKey, data);
+            System.out.println("sign:" + sign);
+            String publicKey = keyPairString.getPublicKey();
+            System.out.println("publicKey:" + publicKey);
+            boolean verify = SignatureUtil.INSTANCE.verify(signatureAlgorithm, publicKey, data, sign);
+            System.out.println("verify:" + verify);
         }
-        messageContainer.append(throwable).append(Symbol.LINE_SEPARATOR.getSymbol());
-        StackTraceElement[] stackTrace = throwable.getStackTrace();
-        for (int i = 0; i < stackTrace.length; i++) {
-            messageContainer.append("    ").append(stackTrace[i]).append(Symbol.LINE_SEPARATOR.getSymbol());
-            if (i > 3) {
-                break;
-            }
-        }
-        return stackTraceToString(throwable.getCause(), messageContainer);
     }
+
+    @Test
+    public void testKey() {
+        for (int i = 6; i < 65; i++) {
+            SecretKeyUtil.INSTANCE.generateKey("DESede",i,"fffff");
+        }
+    }
+
 }
