03423c73b60283eba6cfeffe38f75644a8ff5aab
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-09-08 07:18:56 +0800

    controller返回直接对象，封装格式

--- src/main/java/com/sintrue/matrix/example/domain/Staff.java
+++ src/main/java/com/sintrue/matrix/example/domain/Staff.java
@@ -3,12 +3,13 @@ package com.sintrue.matrix.example.domain;
 import wang.liangchen.matrix.framework.commons.object.ObjectUtil;
 import wang.liangchen.matrix.framework.commons.type.ClassUtil;
 import wang.liangchen.matrix.framework.data.annotation.ColumnJson;
+import wang.liangchen.matrix.framework.data.annotation.ColumnMarkDelete;
 import wang.liangchen.matrix.framework.data.annotation.IdStrategy;
 import wang.liangchen.matrix.framework.data.dao.entity.RootEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -16,15 +17,28 @@ import java.time.LocalDateTime;
  * @author Liangchen.Wang 2022-08-31 22:52
  */
 @Entity(name = "staff")
-@Table(name = "staff")
 public class Staff extends RootEntity {
+    /**
+     * 指定主键和主键生成策略
+     */
     @Id
     @IdStrategy(IdStrategy.Strategy.MatrixFlake)
     private Long staffId;
-    private String staffText;
+    /**
+     * 指定属性对应的列名
+     */
+    @Column(name = "staff_text")
+    private String staffDesc;
     private LocalDateTime createDatetime;
     private LocalDate createDate;
+    /**
+     * 指定逻辑删除的列和值
+     */
+    @ColumnMarkDelete("DELETED")
     private StateEnum state;
+    /**
+     * 自动转换属性和Json
+     */
     @ColumnJson
     private Settings settings;
 
@@ -44,12 +58,12 @@ public class Staff extends RootEntity {
         this.staffId = staffId;
     }
 
-    public String getStaffText() {
-        return staffText;
+    public String getStaffDesc() {
+        return staffDesc;
     }
 
-    public void setStaffText(String staffText) {
-        this.staffText = staffText;
+    public void setStaffDesc(String staffDesc) {
+        this.staffDesc = staffDesc;
     }
 
     public LocalDateTime getCreateDatetime() {
--- src/test/java/com/sintrue/matrix/example/test/cache/StandaloneDaoTest.java
+++ src/test/java/com/sintrue/matrix/example/test/cache/StandaloneDaoTest.java
@@ -12,24 +12,48 @@ import wang.liangchen.matrix.framework.data.pagination.OrderByDirection;
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Liangchen.Wang 2022-08-31 22:55
  */
 @SpringBootTest
-public class StaffDaoTest {
+public class StandaloneDaoTest {
     @Inject
     private StandaloneDao standaloneDao;
 
     @Test
     public void testInsert() {
         Staff staff = new Staff();
-        staff.setStaffText("staff15");
+        // ID根据主键策略生成
+        // staff.setStaffId(0L);
+        staff.setStaffDesc("staff15");
         staff.setCreateDatetime(LocalDateTime.now());
         staff.setCreateDate(LocalDate.now());
+        // 枚举支持
         staff.setState(StateEnum.NORMAL);
+        // 自动转换JSON支持
         staff.setSettings(new Settings("male", LocalDate.of(2000, 1, 1)));
         standaloneDao.insert(staff);
+    }
+
+    @Test
+    public void testInsertBatch() {
+        Staff staff = new Staff();
+        // ID根据主键策略生成
+        // staff.setStaffId(0L);
+        staff.setStaffDesc("staff15");
+        staff.setCreateDatetime(LocalDateTime.now());
+        staff.setCreateDate(LocalDate.now());
+        // 枚举支持
+        staff.setState(StateEnum.NORMAL);
+        // 自动转换JSON支持
+        staff.setSettings(new Settings("male", LocalDate.of(2000, 1, 1)));
+        standaloneDao.insert(new ArrayList<Staff>() {{
+            add(staff);
+            add((Staff) staff.clone());
+        }});
     }
 
     @Test
@@ -37,8 +61,8 @@ public class StaffDaoTest {
         Staff staff = standaloneDao.select(Criteria.of(Staff.class)
                 ._equals(Staff::getStaffId, Staff::getStaffId)
                 ._equals(Staff::getStaffId, 438764791394248808L)
-                ._startWith(Staff::getStaffText, "staff")
-                ._contains(Staff::getStaffText, "staff")
+                ._startWith(Staff::getStaffDesc, "staff")
+                ._contains(Staff::getStaffDesc, "staff")
                 ._between(Staff::getCreateDate, LocalDate.MIN, LocalDate.MAX)
                 ._in(Staff::getState, StateEnum.NORMAL, StateEnum.DELETED)
                 ._isNotNull(Staff::getCreateDatetime)
@@ -57,14 +81,14 @@ public class StaffDaoTest {
                 ._equals(Staff::getStaffId)
                 // * 两个列比较
                 ._equals(Staff::getStaffId, Staff::getStaffId)
-                ._startWith(Staff::getStaffText, "staff")
-                ._contains(Staff::getStaffText, "staff")
+                ._startWith(Staff::getStaffDesc, "staff")
+                ._contains(Staff::getStaffDesc, "staff")
                 ._between(Staff::getCreateDate, LocalDate.MIN, LocalDate.MAX)
                 ._in(Staff::getState, StateEnum.NORMAL, StateEnum.DELETED)
                 ._isNotNull(Staff::getCreateDatetime)
                 ._greaterThan(Staff::getCreateDatetime, LocalDateTime.MIN)
                 // * 指定返回的列
-                .resultFields(Staff::getStaffId, Staff::getStaffText, Staff::getSettings)
+                .resultFields(Staff::getStaffId, Staff::getStaffDesc, Staff::getSettings)
         );
         System.out.println();
     }
@@ -72,13 +96,24 @@ public class StaffDaoTest {
     @Test
     public void testPagination() {
         standaloneDao.pagination(Criteria.of(Staff.class)
-                ._startWith(Staff::getStaffText, "staff")
+                ._startWith(Staff::getStaffDesc, "staff")
                 // 分页
                 .pageNumber(1).pageSize(5)
                 // 排序
                 .orderBy(Staff::getCreateDatetime, OrderByDirection.asc)
                 .orderBy(Staff::getCreateDate, OrderByDirection.desc)
         );
+    }
+
+    @Test
+    public void testIn() {
+        List<Long> ids = new ArrayList<Long>() {{
+            add(1L);
+            add(2L);
+        }};
+        Criteria<Staff> criteria = Criteria.of(Staff.class)._in(Staff::getStaffId, ids);
+        List<Staff> list = standaloneDao.list(criteria);
+        System.out.println();
     }
 
 }
