68249806b0f4fa0a26874d55d3fb598ac556c8af
Liangchen.Wang <sintrue@hotmail.com>
2023-01-08 23:29:58 +0800

    fix jackson serial JsonType

--- src/main/java/com/sintrue/matrix/example/service/ExampleService.java
+++ src/main/java/com/sintrue/matrix/example/service/ExampleService.java
@@ -2,19 +2,23 @@ package com.sintrue.matrix.example.service;
 
 import com.sintrue.matrix.example.dao.entity.Example;
 import com.sintrue.matrix.example.dao.entity.ExampleState;
+import com.sintrue.matrix.example.dao.entity.Staff;
 import com.sintrue.matrix.example.service.message_pl.ExampleRequest;
 import com.sintrue.matrix.example.service.message_pl.ExampleResponse;
+import com.sintrue.matrix.example.service.message_pl.StaffRequest;
 import org.springframework.stereotype.Service;
 import wang.liangchen.matrix.framework.commons.exception.ExceptionLevel;
+import wang.liangchen.matrix.framework.commons.object.ObjectUtil;
 import wang.liangchen.matrix.framework.commons.validation.InsertGroup;
 import wang.liangchen.matrix.framework.commons.validation.UpdateGroup;
 import wang.liangchen.matrix.framework.commons.validation.ValidationUtil;
 import wang.liangchen.matrix.framework.data.dao.StandaloneDao;
 import wang.liangchen.matrix.framework.data.dao.criteria.Criteria;
 import wang.liangchen.matrix.framework.data.dao.criteria.DeleteCriteria;
+import wang.liangchen.matrix.framework.data.dao.criteria.UpdateCriteria;
+import wang.liangchen.matrix.framework.data.pagination.PaginationResult;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -50,23 +54,69 @@ public class ExampleService {
     public void delete(Long staffId) {
         DeleteCriteria<Example> criteria = DeleteCriteria.of(Example.class)
                 // logically deleted
-                .markDelete(Example::getState,ExampleState.DELETED)
+                .markDelete(Example::getState, ExampleState.DELETED)
                 ._equals(Example::getStaffId, staffId);
         this.standaloneDao.delete(criteria);
-    }
-
-    public List<ExampleResponse> list(ExampleRequest request) {
-        Criteria<Example> criteria = Criteria.of(Example.class)._equals(Example::getStaffId, 0L);
-        this.standaloneDao.list(criteria);
-        return Collections.emptyList();
     }
 
     public void update(ExampleRequest request) {
         ValidationUtil.INSTANCE.validate(ExceptionLevel.INFO, request, UpdateGroup.class);
         // validate primary key
-        ValidationUtil.INSTANCE.notEmpty(ExceptionLevel.INFO,request.getStaffId());
+        ValidationUtil.INSTANCE.notEmpty(ExceptionLevel.INFO, request.getStaffId());
         // transform to entity
         Example entity = Example.valueOf(request);
+        // Two ways to set columns that force updates, such as updating to null
+        // entity.addForceUpdateColumn();
+        // entity.addExtendedField();
         this.standaloneDao.update(entity);
+    }
+
+    public int updateByCriteria(ExampleRequest request) {
+        ValidationUtil.INSTANCE.notNull(request);
+        // transform to entity
+        Staff entity = Staff.newInstance();
+        // TODO 将要更新的字段设置到entity
+        // entity.
+        UpdateCriteria<Staff> criteria = UpdateCriteria.of(entity);
+        // 强制更新,比如将值更新为null
+        // .forceUpdate()
+        // TODO 构造更新条件
+        return this.standaloneDao.update(criteria);
+    }
+
+    public ExampleResponse select(Long staffId) {
+        Criteria<Staff> criteria = Criteria.of(Staff.class)
+                // Specify the returned columns
+                //.resultColumns()
+                ._equals(Staff::getStaffId, staffId);
+        Staff entity = this.standaloneDao.select(criteria);
+        if (null == entity) {
+            return null;
+        }
+        //transform Entity to Response
+        return entity.to(ExampleResponse.class);
+    }
+
+    public boolean exists(Long staffId) {
+        Criteria<Staff> criteria = Criteria.of(Staff.class)
+                ._equals(Staff::getStaffId, staffId);
+        return this.standaloneDao.exists(criteria);
+    }
+
+    public List<ExampleResponse> list(ExampleRequest request) {
+        Criteria<Example> criteria = Criteria.of(Example.class);
+        // TODO 构造查询条件
+        List<Example> entities = this.standaloneDao.list(criteria);
+        // return ObjectUtil.INSTANCE.copyProperties(entities, StaffResponse.class, (source, target) -> {});
+        return ObjectUtil.INSTANCE.copyProperties(entities, ExampleResponse.class);
+    }
+
+    public PaginationResult<ExampleResponse> pagination(StaffRequest request) {
+        Criteria<Staff> criteria = Criteria.of(Staff.class);
+        // TODO 构造查询条件
+        PaginationResult<Staff> entityPagination = this.standaloneDao.pagination(criteria);
+        // transform Entity to Response
+        // return entityPagination.to(StaffResponse.class, (source, target) -> {});
+        return entityPagination.to(ExampleResponse.class);
     }
 }
--- src/test/java/com/sintrue/matrix/example/test/MainTest.java
+++ src/test/java/com/sintrue/matrix/example/test/MainTest.java
@@ -1,7 +1,10 @@
 package com.sintrue.matrix.example.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import wang.liangchen.matrix.framework.springboot.jackson.DefaultObjectMapper;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,5 +14,11 @@ public class MainTest {
         Set<String> set = new HashSet<>();
         set.add("a");
         System.out.println(set);
+    }
+    @Test
+    public void testJson() throws JsonProcessingException {
+        LocalDate localDate = LocalDate.now();
+        String string = DefaultObjectMapper.INSTANCE.objectMapper().writeValueAsString(localDate);
+        System.out.println(string);
     }
 }
