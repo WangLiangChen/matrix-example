aed4b65ca2956a5470e52125710f8a6bfc40024e
Liangchen.Wang <sintrue@hotmail.com>
2025-09-09 22:08:45 +0800

    new branch

---
+++ matrix-framework-example/src/main/java/com/sintrue/matrix/framework/example/MatrixFrameworkExample.java
@@ -0,0 +1,12 @@
+package com.sintrue.matrix.framework.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixFrameworkExample {
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(MatrixFrameworkExample.class);
+        springApplication.run(args);
+    }
+}
